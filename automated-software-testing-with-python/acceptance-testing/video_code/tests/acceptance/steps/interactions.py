e75023b77abac412fbc08eaeb06fef1b1a7067a2
Kevin Shields <kshields551@gmail.com>
2022-03-20 12:19:53 -0400

    everything works just going to see if i can change the order

--- automated-software-testing-with-python/acceptance-testing/video_code/tests/acceptance/steps/interactions.py
+++ automated-software-testing-with-python/acceptance-testing/video_code/tests/acceptance/steps/interactions.py
@@ -1,6 +1,7 @@
 from behave import *
 
 from acceptance.page_model.base_page import BasePage
+from acceptance.page_model.new_post_page import NewPostPage
 
 use_step_matcher('re')
 
@@ -15,3 +16,13 @@ def step_impl(context, link_text):
         matching_links[0].click()
     else:
         raise RuntimeError() # we can do something else here if we want like log something to the console then raise the error. Of course raising an exception on any of these tests is going to fail the test. It is going to happen if we use this step somewhere where there is no link or if we spell the link text incorrectly
+
+@when('I enter "(.*)" in the "(.*)" field')
+def step_impl(context, content, field_name):
+    page = NewPostPage(context.driver)
+    page.form_field(field_name).send_keys(content)
+
+@when('I press the submit button')
+def step_imp(context):
+    page = NewPostPage(context.driver)
+    page.submit_button.click()
--- automated-software-testing-with-python/acceptance-testing/video_code/tests/acceptance/steps/navigation.py
+++ automated-software-testing-with-python/acceptance-testing/video_code/tests/acceptance/steps/navigation.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 
 from acceptance.page_model.blog_page import BlogPage
 from acceptance.page_model.home_page import HomePage
+from acceptance.page_model.new_post_page import NewPostPage
 
 use_step_matcher('re')
 
@@ -20,6 +21,12 @@ def step_impl(context):
    page = BlogPage(context.driver)
    context.driver.get(page.url)
 
+@given('I am on the new post page')
+def step_impl(context):
+   context.driver = webdriver.Chrome(r"C:\Users\kshie\Documents\testing\selenium-drivers\chromedriver.exe")
+   page = NewPostPage(context.driver)
+   context.driver.get(page.url)
+
 
 @then('I am on the blog page')
 def step_impl(context):
@@ -30,3 +37,4 @@ def step_impl(context):
 def step_impl(context):
    expected_url = HomePage(context.driver).url
    assert context.driver.current_url == expected_url
+
